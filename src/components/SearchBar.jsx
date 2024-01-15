3b05d055f2d23f94f487348715acddd551f3f5e5
Tymon 'TeeJay' Jędryczka <jedryczkatymon@gmail.com>
2024-01-15 19:50:12 +0100

    SearchBar almost finished

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,37 +1,31 @@
 import '../styles/SearchBar/SearchBar.css'
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Select, { components } from 'react-select';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
-
-library.add(faCaretDown);
-
-const CaretDownIcon = () => {
-    return <FontAwesomeIcon icon="caret-down" />;
-};
-
-const DropdownIndicator = props => {
-    return (
-        <components.DropdownIndicator {...props}>
-            <CaretDownIcon />
-        </components.DropdownIndicator>
-    );
-};
+import magnifyingGlass from "../assets/magnifying-glass.svg";
+import movies from "../assets/movies.svg";
+import tvShows from "../assets/tv-shows.svg";
+import celebs from "../assets/celebs.svg";
+import companies from "../assets/companies.svg";
+import keywords from "../assets/keywords.svg";
+import advancedSearch from "../assets/advanced-search.svg";
 
 const options = [
-    { value: 'all', label: 'All', icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="ipc-icon ipc-icon--magnify" viewBox="0 0 24 24" fill="#8F8F8F" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg> },
-    { value: 'titles', label: 'Titles', icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="ipc-icon ipc-icon--movie" viewBox="0 0 24 24" fill="#8F8F8F" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M18 4v1h-2V4c0-.55-.45-1-1-1H9c-.55 0-1 .45-1 1v1H6V4c0-.55-.45-1-1-1s-1 .45-1 1v16c0 .55.45 1 1 1s1-.45 1-1v-1h2v1c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-1h2v1c0 .55.45 1 1 1s1-.45 1-1V4c0-.55-.45-1-1-1s-1 .45-1 1zM8 17H6v-2h2v2zm0-4H6v-2h2v2zm0-4H6V7h2v2zm10 8h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2V7h2v2z"></path></svg> },
-    { value: 'tv-episodes', label: 'TV Episodes', icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="ipc-icon ipc-icon--television" viewBox="0 0 24 24" fill="#8F8F8F" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v1c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-1h5c1.1 0 1.99-.9 1.99-2L23 5a2 2 0 0 0-2-2zm-1 14H4c-.55 0-1-.45-1-1V6c0-.55.45-1 1-1h16c.55 0 1 .45 1 1v10c0 .55-.45 1-1 1z"></path></svg> },
-    { value: 'celebs', label: 'Celebs', icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="ipc-icon ipc-icon--people" viewBox="0 0 24 24" fill="#8F8F8F" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5s-3 1.34-3 3 1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V18c0 .55.45 1 1 1h12c.55 0 1-.45 1-1v-1.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05.02.01.03.03.04.04 1.14.83 1.93 1.94 1.93 3.41V18c0 .35-.07.69-.18 1H22c.55 0 1-.45 1-1v-1.5c0-2.33-4.67-3.5-7-3.5z"></path></svg> },
-    { value: 'companies', label: 'Companies', icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="ipc-icon ipc-icon--business searchCatSelector__itemIcon" viewBox="0 0 24 24" fill="#8F8F8F" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M12 7V5c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2h-8zM6 19H4v-2h2v2zm0-4H4v-2h2v2zm0-4H4V9h2v2zm0-4H4V5h2v2zm4 12H8v-2h2v2zm0-4H8v-2h2v2zm0-4H8V9h2v2zm0-4H8V5h2v2zm9 12h-7v-2h2v-2h-2v-2h2v-2h-2V9h7c.55 0 1 .45 1 1v8c0 .55-.45 1-1 1zm-1-8h-2v2h2v-2zm0 4h-2v2h2v-2z"></path></svg> },
-    { value: 'keywords', label: 'Keywords', icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="ipc-icon ipc-icon--label searchCatSelector__itemIcon" viewBox="0 0 24 24" fill="#8F8F8F" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M17.63 5.84C17.27 5.33 16.67 5 16 5L5 5.01C3.9 5.01 3 5.9 3 7v10c0 1.1.9 1.99 2 1.99L16 19c.67 0 1.27-.33 1.63-.84l3.96-5.58a.99.99 0 0 0 0-1.16l-3.96-5.58z"></path></svg>},
-    { value: 'advanced-search', label: 'Advanced Search', icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="ipc-icon ipc-icon--find-in-page searchCatSelector__itemIcon" viewBox="0 0 24 24" fill="#8F8F8F" role="presentation"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M20 19.59V8.83c0-.53-.21-1.04-.59-1.41l-4.83-4.83c-.37-.38-.88-.59-1.41-.59H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c.45 0 .85-.15 1.19-.4l-4.43-4.43c-.86.56-1.89.88-3 .82-2.37-.11-4.4-1.96-4.72-4.31a5.013 5.013 0 0 1 5.83-5.61c1.95.33 3.57 1.85 4 3.78.33 1.46.01 2.82-.7 3.9L20 19.59zM9 13c0 1.66 1.34 3 3 3s3-1.34 3-3-1.34-3-3-3-3 1.34-3 3z"></path></svg> },
+    { value: 'all', label: 'All', icon: magnifyingGlass },
+    { value: 'titles', label: 'Titles', icon: movies },
+    { value: 'tv-episodes', label: 'TV Episodes', icon: tvShows },
+    { value: 'celebs', label: 'Celebs', icon: celebs },
+    { value: 'companies', label: 'Companies', icon: companies },
+    { value: 'keywords', label: 'Keywords', icon: keywords },
+    { value: 'advanced-search', label: 'Advanced Search', icon: advancedSearch },
 ];
 
 const styles = {
     control: (provided) => ({
         ...provided,
+        width: "fit-content",
         backgroundColor: "#FFFFFF",
         borderRadius: "7px 0 0 7px",
         height: "30px",
@@ -46,6 +40,11 @@ const styles = {
         ...provided,
         backgroundColor: "#1F1F1F",
         width: "15rem",
+        height: "fit-content",
+    }),
+    menuList: (provided) => ({
+        ...provided,
+        minHeight: "20.75rem"
     }),
     option: (provided, state) => ({
         ...provided,
@@ -57,11 +56,12 @@ const styles = {
             backgroundColor: '#676767'
         },
         cursor: "pointer",
-        fontSize: state.selectProps.myFontSize
+        fontSize: state.selectProps.myFontSize,
     }),
     singleValue: (provided) => ({
         ...provided,
-        fontSize: "17px",
+        fontSize: "14px",
+        padding: "auto auto auto 0.1rem"
     }),
     dropdownIndicator: (provided, state) => ({
         ...provided,
@@ -79,28 +79,83 @@ const styles = {
 };
 
 const SearchBar = () => {
-    return (
-        <>
-            <Select
-                myFontSize="20px"
-                options={options}
-                styles={styles}
-                defaultValue={options[0]}
-                isSearchable={false}
-                components={{ DropdownIndicator }}
-                getOptionLabel={ e => (
-                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                        {e.icon}
-                        <span style={{ marginLeft: 15, fontSize: '1rem' }}>{e.label}</span>
-                    </div>
-                )}
-            />
-            <form id="searchbar">
-                <input className="searchInput" type="text" placeholder="Search IMDb" name="search"></input>
-                <button className="searchButton" type="submit"></button>
-            </form>
-        </>
+    const [selectedOption, setSelectedOption] = useState(options[0]);
+
+    const handleChange = (value) => {
+        setSelectedOption(value);
+    };
+
+    const SingleValue = ({ children, ...props }) => (
+        <components.SingleValue {...props}>
+            {children}
+        </components.SingleValue>
     );
-}
+
+    const Option = (props) => (
+        <components.Option {...props} className="option">
+            <img src={props.data.icon} className="icon" />
+            {props.data.label}
+        </components.Option>
+    );
+
+    library.add(faCaretDown);
+
+    const CaretDownIcon = () => {
+        return <FontAwesomeIcon icon="caret-down" />;
+    };
+
+    const DropdownIndicator = props => {
+        return (
+            <components.DropdownIndicator {...props}>
+                <CaretDownIcon />
+            </components.DropdownIndicator>
+        );
+    };
+
+    useEffect(() => {
+        function addBorder() {
+            document.getElementById("container").style.border = "2px solid #F4C519";
+        }
+
+        function removeBorder() {
+            document.getElementById("container").style.border = "none";
+        }
+        
+        document.querySelector("#searchbar").addEventListener('click', addBorder);
+        document.querySelector("#select").addEventListener("click", removeBorder)
+        document.querySelector(".menu").addEventListener("click", removeBorder)
+        document.querySelector("main").addEventListener("click", removeBorder)
+        document.querySelector("footer").addEventListener("click", removeBorder)
+
+
+        return () => {
+            document.getElementById("searchbar").removeEventListener('focus', addBorder);
+        };
+    }, []);
+
+    return (
+        <div id="container">
+            <div id="select">
+                <Select
+                    value={selectedOption}
+                    onChange={handleChange}
+                    options={options}
+                    styles={styles}
+                    isSearchable={false}
+                    myFontSize="20px"
+                    components={{
+                        DropdownIndicator,
+                        Option,
+                        SingleValue
+                    }}
+                />
+            </div>
+            <form id="searchbar">
+                <input id="searchInput" type="text" placeholder="Search IMDb" name="search"></input>
+                <button id="searchButton" type="submit"></button>
+            </form>
+        </div>
+    );
+};
 
 export default SearchBar;
