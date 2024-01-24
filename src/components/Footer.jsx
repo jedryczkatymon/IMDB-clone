cfac8fa2a64215a0b7e02f9584895fd8c5d39b9d
Tymon 'TeeJay' Jędryczka <jedryczkatymon@gmail.com>
2024-01-24 18:16:38 +0100

    added FooterLinks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
-import "./YellowButton"
 import YellowButton from "./YellowButton"
+import SocialMedia from "./SocialMedia"
+import FooterLinks from "./FooterLinks" 
+import AmazonLogo from "./AmazonLogo"
+import CompanyInfo from "./CompanyInfo"
 
 const Footer = () => {
     return (
         <>
             <YellowButton />
+            <SocialMedia />
+            <FooterLinks />
+            <AmazonLogo />
+            <CompanyInfo />
         </>
     )
 }
---
+++ src/components/FooterLinks.jsx
@@ -0,0 +1,24 @@
+import "../styles/FooterLinks/FooterLinks.css"
+
+const FooterLinks = () => {
+    return (
+        <div id="footerlinks">
+            <ul id="links">
+                <li className="link">Get the IMDb App</li>
+                <li className="link">Help</li>
+                <li className="link">Site Index</li>
+                <li className="link">IMDbPro</li>
+                <li className="link">Box Office Mojo</li>
+                <li className="link">IMDb Developer</li>
+                <li>Press Room</li>
+                <li className="link">Advertising</li>
+                <li className="link">Jobs</li>
+                <li>Conditions of Use</li>
+                <li>Privacy Policy</li>
+                <li className="privacy">Your Ads Privacy Choices</li>
+            </ul>
+        </div>
+    )
+}
+
+export default FooterLinks
